1bd2d35306e59f7da519aa6227d755b56a35df3b
Beiqi Guan <beiqig@andrew.cmu.edu>
2017-10-03 17:54:40 -0400

    boilerplate setup;

---
+++ README.md
@@ -0,0 +1,1 @@
+### Viagogo Programming Test
---
+++ ViagogoTest.java
@@ -0,0 +1,34 @@
+public class ViagogoTest {
+
+  public class Point {
+    public int x;
+    public int y;
+  }
+
+  public class Ticket {
+    private int price;
+
+  }
+  public class Event {
+    private int uid;
+    private Ticket[] m_tickets;
+  }
+
+  private static Event[] m_events;
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+
+  /*** fill m_events with random events ***/
+
+  private static void generateSeedData() {
+    return;
+  }
+
+  /*** Calculate Manhattan Distance ***/
+  private int calculateDistance(Event a, Event b) {
+    return 0;
+  }
+
+}
